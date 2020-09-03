29c8fa6b3174542c4a819dd2fd6952b7aa9078e8
shogo.matsuda <mattunnnn.1024.m@gmail.com>
2020-09-03 03:57:51 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @micropost = current_user.tasklists.build  # form_with 用
+      @microposts = current_user.tasklists.order(id: :desc).page(params[:page])
+    end
   end
 end
